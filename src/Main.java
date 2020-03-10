f8f2b4e1311ea97dd883e2a81bbfa8c19ab6f4bc
55640281 <55640281+ericjanto@users.noreply.github.com>
2020-03-10 14:41:44 +0000

    Test if pushing code to branch works.

--- src/Main.java
+++ src/Main.java
@@ -8,5 +8,6 @@ public class Main {
     public static void main(String[] args) {
         LibraryBrowser browser = new LibraryBrowser();
         browser.run();
+        System.out.println();
     }
 }
